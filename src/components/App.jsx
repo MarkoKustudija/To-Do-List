6b0fdef70088c322ada678059df68f7236a5673b
Marko Kustudija <markokustudija@Markos-MacBook-Air.local>
2021-05-19 17:28:38 +0200

    adding InputArea file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,17 @@
 import React, { useState } from "react";
 import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
 
 function App() {
-  const [inputText, setInputText] = useState("");
   const [items, setItems] = useState([]);
 
-  function handleChange(event) {
-    const newValue = event.target.value;
-    setInputText(newValue);
-  }
-
-  function addItem() {
+  function addItem(inputText) {
     setItems((prevItems) => {
       return [...prevItems, inputText];
     });
-    setInputText("");
   }
 
   function deletedItem(id){
-    // console.log("Deleted");
     setItems( prevItems => {
       return prevItems.filter((item, index) => {
         return index !== id;
@@ -31,12 +24,7 @@ function App() {
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <input onChange={handleChange} type="text" value={inputText} />
-        <button onClick={addItem}>
-          <span>Add</span>
-        </button>
-      </div>
+      <InputArea onAdd = {addItem} />
       <div>
         <ul>
           {items.map((todoItem, index) => (
